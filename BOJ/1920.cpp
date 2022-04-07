88548dec32339990b7956857e4ab3350cd8057d4
dongchan0330 <dongchan0330@naver.com>
2022-04-07 10:36:57 +0900

    Add 1920

---
+++ BOJ/1920.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n;
+	set<int>s;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		s.insert(num);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		auto it = s.find(num);
+		if (it != s.end())	cout << "1" << "\n";
+		else cout << "0" << "\n";
+	}
+}
