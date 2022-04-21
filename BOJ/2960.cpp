b151dc68a94275c8a1ade951f9ad1e0f9df263e0
dongchan0330 <dongchan0330@naver.com>
2022-04-21 23:31:21 +0900

    Add 2960

---
+++ BOJ/2960.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+bool isprime(int n) {
+	if (n == 1)	return false;
+	for (int i = 2; i <= sqrt(n); i++) {
+		if ((n % i) == 0)	return false;
+	}
+	return true;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int>list;
+	vector<int>save_list;
+	for (int i = 2; i <= n; i++)	list.push_back(i);
+	int num = 2;
+	while (1) {
+		if (save_list.size() == n - 1)	break;
+		if (isprime(num)) {
+			for (int i = 0; i < list.size(); i++) {
+				if (list[i] % num == 0 && list[i] != 0) {
+					save_list.push_back(list[i]);
+					list[i] = 0;
+				}
+			}
+		}
+		num++;
+	}
+	cout << save_list[k - 1];
+}
